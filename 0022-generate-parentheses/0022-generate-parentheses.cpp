133fe7b909cd98ff694ef1025c4501359711538f
ananya0609 <gargananya0609@gmail.com>
2026-08-03 23:51:03 +0530

    Time: 3 ms (69.32%), Space: 16 MB (32.2%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    vector<string>ans; 
-     void solve(string s, int open, int close, int n) {
-    if(s.length() ==2*n){
-      ans.push_back(s);
-      return;
-    }
-    if(open<n){
-         solve(s + "(", open + 1, close, n);
+    vector<string> ans;
 
-    }
-    if(close<open){
-         solve(s + ")", open , close + 1, n);
+    void solve(string s, int open, int close, int n) {
 
+        // If the string has 2*n characters, it is complete
+        if (s.length() == 2 * n) {
+            ans.push_back(s);
+            return;
+        }
+
+        // Add '(' if we still have opening brackets left
+        if (open < n) {
+            solve(s + "(", open + 1, close, n);
+        }
+
+        // Add ')' only if it keeps the string valid
+        if (close < open) {
+            solve(s + ")", open, close + 1, n);
+        }
     }
-     }
+
     vector<string> generateParenthesis(int n) {
         solve("", 0, 0, n);
         return ans;
